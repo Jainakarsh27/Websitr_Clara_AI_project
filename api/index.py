4abfe2626d4da8ac0b2ffe3d9687c14d5dc9178d
Jainakarsh202728 <147416424+Jainakarsh27@users.noreply.github.com>
2026-03-05 21:51:56 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -9,7 +9,3 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 @app.get("/")
 def home():
     return FileResponse(os.path.join(BASE_DIR, "public", "index.html"))
-
-@app.get("/api")
-def api():
-    return {"message": "Clara AI API running"}
