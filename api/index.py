b6d27e199c9c9e9c798b556a42e1bb3fffe6824f
Jainakarsh202728 <147416424+Jainakarsh27@users.noreply.github.com>
2026-03-05 11:56:32 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -2,6 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/api")
 def home():
-    return {"message": "Clara AI is running"}
+    return {"message": "Clara AI API is running"}
