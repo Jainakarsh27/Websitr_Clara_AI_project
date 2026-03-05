728f4b6e1445249522ed2dff68dccf89164ef914
Jainakarsh202728 <147416424+Jainakarsh27@users.noreply.github.com>
2026-03-05 21:18:40 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
+import os
 
 app = FastAPI()
+
+@app.get("/")
+def homepage():
+    return FileResponse("public/index.html")
 
 @app.get("/api")
 def api():
