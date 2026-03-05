572dc626ffce4975bff0e1b8bed236b1b77d04f2
Jainakarsh202728 <147416424+Jainakarsh27@users.noreply.github.com>
2026-03-05 12:26:52 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -2,10 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return {"message": "Clara AI API running"}
-
 @app.get("/api")
 def api():
-    return {"message": "Clara AI endpoint working"}
+    return {"message": "Clara AI API running"}
